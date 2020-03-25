473adbbc9f58232dde2cf08f5f49e0d3a2637a06
DarkArmy00 <49871840+DarkArmy00@users.noreply.github.com>
2020-03-25 20:43:56 +0200

    Update

--- admin/includes/templates/footer.php
+++
@@ -1,7 +0,0 @@
-        <?php $js = 'layout/js/'; ?>
-        <script src="<?php echo $js; ?>all.min.js"></script>
-        <script src="<?php echo $js; ?>jQuery.v3.4.1.js"></script>
-        <script src="<?php echo $js; ?>bootstrap.min.js"></script>
-        <script src="<?php echo $js; ?>main.js"></script>
-    </body>
-</html>
